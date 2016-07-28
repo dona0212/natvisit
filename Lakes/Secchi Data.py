c29a2286d1aef58077df21ae7745bda4d21b07d7
Ryan <ryannoe@gmail.com>
2016-07-28 10:09:49 -0400

    Added a method to limit average to certain months

--- Lakes/Secchi Data.py
+++ Lakes/Secchi Data.py
@@ -3,7 +3,17 @@ import pandas as pd
 pd.set_option('display.width', 500)
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
-secchiframe = pd.read_table('/Users/thom2117/Documents/SCIENCE/LAGOS/DavesData/lagos/LAGOS_LIMNO_v1.054.1/lagos_secchi_10541.txt')
-print secchiframe
+months_subset = [5,6,7]
 
-print secchiframe.groupby('lagoslakeid').mean()
+
+secchi = pd.read_table('/Users/thom2117/Documents/SCIENCE/LAGOS/DavesData/lagos/LAGOS_LIMNO_v1.054.1/lagos_secchi_10541.txt')
+# secchi = pd.read_table('C:/Users/rrnoe/NatCap/SESYNC/LAGOS_LIMNO_v1.054.1/lagos_secchi_10541.txt')
+
+# print secchi
+subset_secchi = secchi[secchi['samplemonth'].isin(months_subset)]
+
+
+
+grouped_subset_secchi = subset_secchi.groupby('lagoslakeid').mean()['secchi']
+print grouped_subset_secchi
+
